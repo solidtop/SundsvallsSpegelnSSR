37a7b2c6f4c57221fb833ced81aafc93ec6677ed
Axel Asp <solidtop.asp@gmail.com>
2023-01-25 14:38:44 +0100

    Add Markdown support for movie intros

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,22 @@
 import express from 'express';
+import { marked } from 'marked';
 import { fetchMovies, fetchMovie } from './movies.js'; 
 import getDropdownMenu from './menus.js';
+import getMovieTemplates from './templates.js'
 import { sortByDate } from './sorting.js';
 
 const router = express.Router();
 
 const DROPDOWN_MENU = getDropdownMenu();
+const MOVIE_TEMPLATES = getMovieTemplates();
 
 //Homepage 
 router.get('/', async (req, res) => {
     const movies = await fetchMovies();
     const sortedMovies = sortByDate(movies);
 
-    const amountToDisplay = 10;
-    const moviesOutNow = movies.length > amountToDisplay ? movies.slice(0, amountToDisplay) : movies; //Slice first x movies to display
+    const moviesToDisplay = 10;
+    const moviesOutNow = movies.length > moviesToDisplay ? movies.slice(0, moviesToDisplay) : movies; //Slice first x movies to display
   
     res.render('index', {
         title: 'Sundsvalls Spegeln, Bio-Bar-Bistro',
@@ -38,9 +41,10 @@ router.get('/movies/:id', async (req, res) => {
         res.render('movie', {
             title: movie.attributes.title,
             dropdownMenu: DROPDOWN_MENU,
-            movie: movie
-        });
-    } else {
+            movie: movie,
+            md: marked //Send marked function to pug file so it can use markdown
+        });            //NOTE: It works but maybe not the best implementation if using md in more templates.. Couldn't find any other solution when using variables/dynamic text
+    } else {           
         res.status(404).render('404', {
             dropdownMenu: DROPDOWN_MENU
         });
